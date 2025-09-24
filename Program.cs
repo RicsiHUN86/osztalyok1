1889f907b72bcfc7e5dad18b10e126f20b50e1e2
BRicsi <barnusrichi@gmail.com>
2025-09-24 07:52:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osztalyok1
+{
+    internal class Program
+    {
+        public class Butorlap
+        {
+            private double hossz;
+            private double szelesseg;
+            private string tipus;
+
+            public const int TARTOLAP_AR = 5000;
+            public const int HATLAP_AR = 500;
+
+        }
+            static void Main(string[] args)
+        {
+        }
+    }
+}
